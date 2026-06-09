9a05dc04ee7d9fa7e7b868a73bc5884ccf3d0c96
Akhil Joseph <ajpulluvelil@gmail.com>
2026-06-09 15:25:54 +0530

    JSCQ-16 - Given an array of strings. Return an array with the angrams as sub arrays.

--- scripts/JSCQ-15/index.js
+++ scripts/JSCQ-15/index.js
@@ -2,18 +2,17 @@ function groupAnagrams(words) {
     const result = {};
 
     for (const word of words) {
-        const sortedWord = word.toLowerCase().split('').sort().join('');
+        const sortedWord = word.toLowerCase().replace(/[^a-z0-9]/g, '').split('').sort().join('');
         console.log(sortedWord);
         if (result[sortedWord]) {
             result[sortedWord].push(word);
         } else {
             result[sortedWord] = [word]
         }
-
     }
 
     return Object.values(result);
 }
 
-const words = ['Ate', 'Eat', 'Hello', 'Integral', 'Listen', 'Silent', 'Tea', 'Triangle', 'World'];
+const words = ['Ate ', 'Eat', 'Hello', 'Integral', 'Listen', 'Silent', 'Tea', 'Triangle', 'World'];
 console.log(groupAnagrams(words));
