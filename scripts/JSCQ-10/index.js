6464f2a1be2ea998ee896f9335ff3e3eba9b84c8
Akhil Joseph <ajpulluvelil@gmail.com>
2026-06-15 09:29:51 +0530

    JSCQ-10 - Typo fixed.

--- scripts/JSCQ-10/index.js
+++ scripts/JSCQ-10/index.js
@@ -1,6 +1,6 @@
-function removeDupicates(numbers) {
+function removeDuplicates(numbers) {
     return [...new Set(numbers)];
 }
 
 const numbers = [1, 2, 3, 2, 4, 5, 6, 8];
-console.log(removeDupicates(numbers));
+console.log(removeDuplicates(numbers));
